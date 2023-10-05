a44fb3d36261a793aac851b07c0e3739389e1c53
Steffanie <steflopes@icloud.com>
2023-10-05 19:02:30 -0300

    Criação React

---
+++ blog-pessoal/src/App.tsx
@@ -0,0 +1,14 @@
+import Home from './paginas/home/Home';
+import './App.css';
+import React from 'react';
+
+function App() {
+ 
+  return (
+    <>
+      <Home/>
+    </>
+  );
+}
+
+export default App;
